dbc49a908cee01acfdef99c94e52a134dc79750c
Phamduc <phamduc0701@gmail.com>
2020-10-08 08:45:17 +0700

    - Rename BasicTimer.py -> TimerBasic.py\n+ ContexInMultiprocess.py(WIP)

---
+++ ContextInMultiprocess.py
@@ -0,0 +1,24 @@
+import multiprocessing as mp
+import time
+
+
+def foo(queue):
+    queue.put(["Hello", "World"])
+
+
+def bar(queue):
+    queue.put(["The", "Duc"])
+
+
+if __name__ == "__main__":
+    DEFAULT_START_METHOD = "spawn"
+    context = mp.get_context(DEFAULT_START_METHOD)
+    queue = context.Queue(3)
+    proc = context.Process(target=foo, args=(queue,))
+    proc2 = context.Process(target=bar, args=(queue,))
+    proc.start()
+    proc2.start()
+    # print(queue.get())
+    proc.join()
+    proc2.join()
+    print(queue.get())
